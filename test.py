3b6e16218256a573224be49f2692e76ab84dc9a9
unknown <juliano8103@hotmail.com>
2022-08-27 17:06:01 +0200

    Modification du Code

--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-
-  from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
-
-  cmc = CoinMarketCapAPI('{YOUR_API_KEY}')
-  
-  r = cmc.cryptocurrency_info(symbol='BTC')
-
-  do_something(r.data)
+from coinmarketcapapi import CoinMarketCapAPI, CoinMarketCapAPIError
+cmc = CoinMarketCapAPI('49ea16ef-e9e7-4d65-bb31-5e5244dd8226')
+r = cmc.cryptocurrency_info(symbol='BTC')
+print(r)
