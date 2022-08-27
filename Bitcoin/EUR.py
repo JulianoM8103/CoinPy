00306829a5126358224434285536f4774c5fe2e1
unknown <juliano8103@hotmail.com>
2022-08-27 21:56:04 +0200

    Modification du Code

--- Bitcoin/EUR.py
+++ Bitcoin/EUR.py
@@ -1,4 +1,6 @@
 import requests
 response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 data = response.json()
-print(data["bpi"]["EUR"]["rate"])
+bitcoinprice = (data["bpi"]["EUR"]["rate"])
+
+print("Le prix du Bitcoin est actuellement de",bitcoinprice,"€")
--- Bitcoin/USD.py
+++ Bitcoin/USD.py
@@ -1,4 +1,6 @@
 import requests
 response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 data = response.json()
-print(data["bpi"]["USD"]["rate"])
+bitcoinprice = (data["bpi"]["USD"]["rate"])
+
+print("Le prix du Bitcoin est actuellement de",bitcoinprice,"$")
