704b34b69eaf35a04ef8a908abbd3e3d0e4a87b9
unknown <juliano8103@hotmail.com>
2022-08-28 10:02:59 +0200

    Modification du Code

--- Ethereum/USD.py
+++ Ethereum/USD.py
@@ -1,18 +1,14 @@
-# Import libraries
+
 import json
 import requests
 
-# Defining Binance API URL
 key = "https://api.binance.com/api/v3/ticker/price?symbol="
 
-# Making list for multiple crypto's
 currencies = ["ETHUSD"]
 j = 0
 
-# running loop to print all crypto prices
 for i in currencies:
 
-# completing API for request
  url = key+currencies[j]
 data = requests.get(url)
 data = data.json()
